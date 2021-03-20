84c2ce97cdb1a9c94eed3536752f5aa30fd70b3b
ArturoSerranoDev <asm.arturo.developer@gmail.com>
2021-03-20 12:16:54 +0100

    Created base folder structure.

---
+++ Assets/Scripts/Data/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------------
+// EnemyData.cs
+//
+// Author: Arturo Serrano
+// Date: 20/02/21
+//
+// Brief: Holds necessary data for enemies
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+public class EnemyData
+{
+    public Position position;
+    public int lives;
+}
+
+public struct Position
+{
+    public int x;
+    public int y;
+}
